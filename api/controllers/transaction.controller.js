8c80eb945b410ad11def05e9cd1c51c8d4c4dcff
@tuanthanh94 <tuanthanh021094@gmail.com>
2020-05-06 13:41:07 +0700

    edit return data transactions

--- api/controllers/transaction.controller.js
+++ api/controllers/transaction.controller.js
@@ -3,5 +3,19 @@ const Book = require("../../models/book.model.js");
 const User = require("../../models/user.model.js");
 
 module.exports.index = async (req, res) => {
-  res.json(await Transaction.find());
+  let books = await Book.find();
+  let users = await User.find();
+  let transactions = await Transaction.find();
+
+  let matchTrans = transactions.map(trans => {
+    let book = books.find(book => book.id === trans.bookId.toString());
+    let user = users.find(user => user.id === trans.userId.toString());
+    
+    return { 
+        bookTitle: book.title, 
+        userName: user.name,
+        isComplete : trans.isComplete    
+    };
+});
+  res.json(matchTrans);
 };

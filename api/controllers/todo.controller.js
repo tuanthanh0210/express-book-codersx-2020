ab1a437b8e8b7626f0df7f1cfe45f25a476712f1
@tuanthanh94 <tuanthanh021094@gmail.com>
2020-05-21 13:17:26 +0700

    return todo data

--- api/controllers/todo.controller.js
+++ api/controllers/todo.controller.js
@@ -4,7 +4,7 @@ module.exports.index = async (req, res) => {
   let todos = await Todo.find();
   let returnTodo = todos.map(todo => {
     return {
-      id: todo.id,
+      id: todo['_id'],
       title: todo.title,
       isComplete: todo.isComplete
     }
@@ -12,29 +12,50 @@ module.exports.index = async (req, res) => {
   res.json(returnTodo);
 };
 
-module.exports.search = async (req,res) => {
+module.exports.search = async (req, res) => {
   let todos = await Todo.find()
   let q = req.query.q;
-  let filterTodos = todos.filter( todo => 
-      todo.title.toLowerCase().indexOf(q.toLowerCase()) !== -1 
-  );
+  let filterTodos = todos.filter(todo =>
+    todo.title.toLowerCase().indexOf(q.toLowerCase()) !== -1
+  ).map(todo => {
+    return {
+      id: todo['_id'],
+      title: todo.title,
+      isComplete: todo.isComplete
+    }
+  });
   res.json(filterTodos)
 }
 
 module.exports.create = async (req, res) => {
   await Todo.create(req.body);
   let todos = await Todo.find()
-  res.json(todos);
+
+  let returnTodo = todos.map(todo => {
+    return {
+      id: todo['_id'],
+      title: todo.title,
+      isComplete: todo.isComplete
+    }
+  })
+  res.json(returnTodo);
 };
 
-module.exports.update = async (req,res) => {
-    let id = req.params.id;
-    await Todo.findByIdAndUpdate(id, {
-        // title: req.body.title,
-        isComplete: true
-    })
-    let todos = await Todo.find();
-    res.json(todos)
+module.exports.update = async (req, res) => {
+  let id = req.params.id;
+  await Todo.findByIdAndUpdate(id, {
+    // title: req.body.title,
+    isComplete: true
+  })
+  let todos = await Todo.find();
+  let returnTodo = todos.map(todo => {
+    return {
+      id: todo['_id'],
+      title: todo.title,
+      isComplete: todo.isComplete
+    }
+  })
+  res.json(returnTodo);
 }
 
 module.exports.delete = async (req, res) => {
@@ -43,5 +64,12 @@ module.exports.delete = async (req, res) => {
   await Todo.findByIdAndRemove(id);
   let todos = await Todo.find()
 
-  res.json(todos);
+  let returnTodo = todos.map(todo => {
+    return {
+      id: todo['_id'],
+      title: todo.title,
+      isComplete: todo.isComplete
+    }
+  })
+  res.json(returnTodo);
 };
